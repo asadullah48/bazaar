8e871d32b75c0f120e420cc4538712f37877309d
ASADULLAH SHAFIQUE <asadullahshafique@hotmail.com>
2026-05-12 16:23:39 +0500

    feat(i18n): add ur locale to routing config

--- frontend/src/i18n/routing.ts
+++ frontend/src/i18n/routing.ts
@@ -1,7 +1,7 @@
 import { defineRouting } from "next-intl/routing";
 
 export const routing = defineRouting({
-  locales: ["en", "ar"],
+  locales: ["en", "ar", "ur"],
   defaultLocale: "en",
   localePrefix: "always",
 });
